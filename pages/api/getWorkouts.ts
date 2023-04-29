94c1e49518d43b5dfbcf3af2323e91e50b024fbe
Kalle Moen <hello@kallemoen.com>
2023-04-29 11:30:35 +0100

    Revert "Revert "Initial commit""
    This reverts commit e433c1a07dd1649c35b406c95998182420878f74.

---
+++ pages/api/getWorkouts.ts
@@ -0,0 +1,28 @@
+// pages/api/getWorkouts.js
+import axios from 'axios';
+
+const apiKey = process.env['AIRTABLE_API_KEY']
+const baseId = process.env['AIRTABLE_BASE_ID']
+const tableId = process.env['AIRTABLE_TABLE_ID']
+const airtableUrl = `https://api.airtable.com/v0/${baseId}/${tableId}`;
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Method not allowed' });
+    return;
+  }
+
+  try {
+    const response = await axios.get(airtableUrl, {
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Airtable request failed:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
